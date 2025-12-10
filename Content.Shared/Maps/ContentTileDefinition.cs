879952d663d699bb420b02cd34efca3a8df0f1d9
Ilya246 <57039557+Ilya246@users.noreply.github.com>
2025-12-11 00:20:10 +0400

    The edger

--- Content.Shared/Maps/ContentTileDefinition.cs
+++ Content.Shared/Maps/ContentTileDefinition.cs
@@ -4,11 +4,13 @@ using Content.Shared.Movement.Systems;
 using Content.Shared.Tools;
 using Robust.Shared.Audio;
 using Robust.Shared.Map;
+using Robust.Shared.Maths; // Mono
 using Robust.Shared.Prototypes;
 using Robust.Shared.Serialization;
 using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
 using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype.Array;
 using Robust.Shared.Utility;
+using System.Numerics; // Mono
 
 namespace Content.Shared.Maps
 {
@@ -105,6 +107,20 @@ namespace Content.Shared.Maps
         /// </summary>
         [DataField("mobFriction")]
         public float? MobFriction { get; private set; }
+
+        /// <summary>
+        ///     No-input friction override for mob mover in <see cref="SharedMoverController"/>
+        /// </summary>
+        [DataField("mobFrictionNoInput")]
+        public float? MobFrictionNoInput { get; private set; }
+
+        // <Mono>
+        /// <summary>
+        /// Vertices for drawing purposes. Has to be a convex shape.
+        /// </summary>
+        [DataField]
+        public List<Vector2> Vertices = new() { Vector2.Zero, new Vector2(0, 1), new Vector2(1, 1), new Vector2(1, 0) };
+        // </Mono>
 
         /// <summary>
         ///     Accel override for mob mover in <see cref="SharedMoverController"/>
