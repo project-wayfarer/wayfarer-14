61b5f58a9603456a7f97a85cd8429e3f2a7e014b
VVersengt <jmthornell40@gmail.com>
2026-04-27 19:25:14 +1000

    tweakiin
    Added melee weapons to Grenadiers and commented out the harmless ones.
    Cleaned up some pathfinding code.
    Increased prying speed of mobs.
    Removed turret idle spin.
    Removed inherent gravity of UIVs to make it actually work.

--- Content.Server/NPC/Pathfinding/PathfindingSystem.cs
+++ Content.Server/NPC/Pathfinding/PathfindingSystem.cs
@@ -188,8 +188,8 @@ namespace Content.Server.NPC.Pathfinding
         /// </summary>
         public bool TryCreatePortal(EntityCoordinates coordsA, EntityCoordinates coordsB, out int handle)
         {
-            var mapUidA = _transform.GetMap(coordsA);
-            var mapUidB = _transform.GetMap(coordsB);
+            var mapUidA = coordsA.GetMapUid(EntityManager); // Misfits
+            var mapUidB = coordsB.GetMapUid(EntityManager); // Misfits
             handle = -1;
 
             if (mapUidA != mapUidB || mapUidA == null)
@@ -197,8 +197,8 @@ namespace Content.Server.NPC.Pathfinding
                 return false;
             }
 
-            var gridUidA = _transform.GetGrid(coordsA);
-            var gridUidB = _transform.GetGrid(coordsB);
+            var gridUidA = coordsA.GetGridUid(EntityManager); // Misfits
+            var gridUidB = coordsB.GetGridUid(EntityManager); // Misfits
 
             if (!TryComp<GridPathfindingComponent>(gridUidA, out var gridA) ||
                 !TryComp<GridPathfindingComponent>(gridUidB, out var gridB))
@@ -236,8 +236,8 @@ namespace Content.Server.NPC.Pathfinding
 
             _portals.Remove(handle);
 
-            var gridUidA = _transform.GetGrid(portal.CoordinatesA);
-            var gridUidB = _transform.GetGrid(portal.CoordinatesB);
+            var gridUidA = portal.CoordinatesA.GetGridUid(EntityManager); // Misfits
+            var gridUidB = portal.CoordinatesB.GetGridUid(EntityManager); // Misfits
 
             if (!TryComp<GridPathfindingComponent>(gridUidA, out var gridA) ||
                 !TryComp<GridPathfindingComponent>(gridUidB, out var gridB))
@@ -264,7 +264,7 @@ namespace Content.Server.NPC.Pathfinding
             int limit = 40,
             PathFlags flags = PathFlags.None)
         {
-            if (!TryComp(entity, out TransformComponent? start))
+            if (!TryComp<TransformComponent>(entity, out var start)) // Misfits
                 return new PathResultEvent(PathResult.NoPath, new List<PathPoly>());
 
             var layer = 0;
@@ -294,7 +294,7 @@ namespace Content.Server.NPC.Pathfinding
             CancellationToken cancelToken,
             PathFlags flags = PathFlags.None)
         {
-            if (!TryComp(entity, out TransformComponent? start))
+            if (!TryComp<TransformComponent>(entity, out var start)) // Misfits
                 return null;
 
             var request = GetRequest(entity, start.Coordinates, end, range, cancelToken, flags);
@@ -325,8 +325,8 @@ namespace Content.Server.NPC.Pathfinding
             CancellationToken cancelToken,
             PathFlags flags = PathFlags.None)
         {
-            if (!TryComp(entity, out TransformComponent? xform) ||
-                !TryComp(target, out TransformComponent? targetXform))
+            if (!TryComp<TransformComponent>(entity, out var xform) || // Misfits
+                !TryComp<TransformComponent>(target, out var targetXform)) // Misfits
                 return new PathResultEvent(PathResult.NoPath, new List<PathPoly>());
 
             var request = GetRequest(entity, xform.Coordinates, targetXform.Coordinates, range, cancelToken, flags);
@@ -397,22 +397,22 @@ namespace Content.Server.NPC.Pathfinding
         /// </summary>
         public PathPoly? GetPoly(EntityCoordinates coordinates)
         {
-            var gridUid = _transform.GetGrid(coordinates);
+            var gridUid = coordinates.GetGridUid(EntityManager); // Misfits
 
             if (!TryComp<GridPathfindingComponent>(gridUid, out var comp) ||
-                !TryComp(gridUid, out TransformComponent? xform))
+                !TryComp<TransformComponent>(gridUid, out var xform)) // Misfits
             {
                 return null;
             }
 
-            var localPos = Vector2.Transform(_transform.ToMapCoordinates(coordinates).Position, _transform.GetInvWorldMatrix(xform));
+            var localPos = Vector2.Transform(coordinates.ToMapPos(EntityManager, _transform), xform.InvWorldMatrix); // Misfits
             var origin = GetOrigin(localPos);
 
             if (!TryGetChunk(origin, comp, out var chunk))
                 return null;
 
             var chunkPos = new Vector2(MathHelper.Mod(localPos.X, ChunkSize), MathHelper.Mod(localPos.Y, ChunkSize));
-            var polys = chunk.Polygons[(int)chunkPos.X * ChunkSize + (int)chunkPos.Y];
+            var polys = chunk.Polygons[(int) chunkPos.X * ChunkSize + (int) chunkPos.Y]; // Misfits
 
             foreach (var poly in polys)
             {
