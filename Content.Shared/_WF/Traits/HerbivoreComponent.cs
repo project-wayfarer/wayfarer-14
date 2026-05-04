21ad81db73d1c596e276c2e3d9039449a3d233c7
Khaomi <46065349+Khaomi@users.noreply.github.com>
2026-05-04 14:20:23 +0700

    docs(traits): fix herbivore trait comment

--- Content.Shared/_WF/Traits/HerbivoreComponent.cs
+++ Content.Shared/_WF/Traits/HerbivoreComponent.cs
@@ -3,7 +3,7 @@ using Robust.Shared.GameStates;
 namespace Content.Shared._WF.Traits;
 
 /// <summary>
-/// This is used for the carnivore trait.
+/// This is used for the herbivore trait.
 /// </summary>
 [RegisterComponent, NetworkedComponent]
 public sealed partial class HerbivoreComponent : Component;
