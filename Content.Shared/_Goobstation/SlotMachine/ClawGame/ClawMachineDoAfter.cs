0fde7b34eee86d71fe8bcdd54e66774ce17e7fe3
Shawn McHenry <drraccoony@gmail.com>
2026-05-18 12:37:36 -0500

    Revert "Goob Gamba Port (#913)"
    This reverts commit 7508a081792cd43a5318f45c7062f2d9ef81f63c.

--- Content.Shared/_Goobstation/SlotMachine/ClawGame/ClawMachineDoAfter.cs
+++
@@ -1,7 +0,0 @@
-using Content.Shared.DoAfter;
-using Robust.Shared.Serialization;
-
-namespace Content.Shared._Goobstation.SlotMachine.ClawGame;
-
-[Serializable, NetSerializable]
-public sealed partial class ClawGameDoAfterEvent : SimpleDoAfterEvent;
