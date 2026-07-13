b636d03ac54b7c709016798128bbd8a8fcdd3ff6
Baublehoard <baublehoard@outlook.com>
2026-07-13 10:52:05 -0300

    switches date to use datetime instead of string

--- Content.Server.Database/Model.cs
+++ Content.Server.Database/Model.cs
@@ -2020,7 +2020,7 @@ namespace Content.Server.Database
 
         [Required] public string Content { get; set; } = string.Empty;
 
-        [Required] public string Date { get; set; } = string.Empty;
+        [Required] public DateTime Date { get; set; }
 
         [Required] public Guid AuthorPlayerUserId { get; set; }
     }
