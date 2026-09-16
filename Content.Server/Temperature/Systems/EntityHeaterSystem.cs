f8e5b79a88024382efe7225e2e3baebcc08f2b24
Roy Aster <sureshotxroy@gmail.com>
2026-09-16 18:50:15 -0500

    Ported Campfire Cooking from Omu
    We only need the C# here, not their RMC campfire.
    
    Co-Authored-By: Quill <292788791+quillthecreature@users.noreply.github.com>

--- Content.Server/Temperature/Systems/EntityHeaterSystem.cs
+++ Content.Server/Temperature/Systems/EntityHeaterSystem.cs
@@ -38,7 +38,7 @@ public sealed class EntityHeaterSystem : SharedEntityHeaterSystem
             // don't divide by total entities since it's a big grill
             // excess would just be wasted in the air but that's not worth simulating
             // if you want a heater thermomachine just use that...
-            var energy = (power.PowerReceived - heater.PassivePower) * deltaTime; // Frontier: subtract PassivePower
+            var energy = (power.NeedsPower ? power.PowerReceived : power.Load) * deltaTime; // Wayfarer: "Frontier: subtract PassivePower"<"Omu When NeedsPower is off, ignore how much power is received and force in the requested amount"
             foreach (var ent in placer.PlacedEntities)
             {
                 _temperature.ChangeHeat(ent, energy);
